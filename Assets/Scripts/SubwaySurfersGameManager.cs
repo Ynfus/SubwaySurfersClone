0f6d55eb245a69b1d989a7f2579dded7c68bcf85
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-02-11 00:27:54 +0100

    GameManager

---
+++ Assets/Scripts/SubwaySurfersGameManager.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SubwaySurfersGameManager : MonoBehaviour
+{
+
+    public static SubwaySurfersGameManager Instance { get; private set; }
+
+    public event EventHandler OnStateChanged;
+    public event EventHandler OnGameUnpaused;
+    public event EventHandler OnGamePaused;
+
+    private void Awake()
+    {
+        Instance = this;
+        state = State.WaitingToStart;
+    }
+    private enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver
+    }
+    private State state;
+    private float countdownTimer = 30f;
+    private float gamePlayingTimer;
+    private float gamePlayingTimerMax = 100f;
+    private bool isGamePaused = false;
+
+    private void Start()
+    {
+        GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
+        //if (state == State.WaitingToStart)
+        //{
+            
+        //    state = State.CountdownToStart;
+        //    OnStateChanged?.Invoke(this, new EventArgs());
+        //}
+        //GameInput.Instance.OnPauseAction += SubwaySurfersGameManager_OnPauseAction;
+    }
+
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        if (state == State.WaitingToStart)
+        {
+            state = State.CountdownToStart;
+            OnStateChanged?.Invoke(this, new EventArgs());
+        }
+    }
+
+    private void SubwaySurfersGameManager_OnPauseAction(object sender, EventArgs e)
+    {
+        TogglePauseGame();
+    }
+
+    public void TogglePauseGame()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+
+    private void Update()
+    {
+        Debug.Log(state);
+        switch (state)
+        {
+            case State.WaitingToStart:
+
+                break;
+            case State.CountdownToStart:
+                countdownTimer -= Time.deltaTime;
+
+                if (countdownTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                Debug.Log("111");
+                if (gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GameOver:
+                break;
+
+        }
+    }
+    public bool IsGamePlaying()
+    {
+        return state == State.GamePlaying;
+    }
+    public bool IsCountdownToStartActive()
+    {
+        Debug.Log(state);
+        return state == State.CountdownToStart;
+    }
+    public float GetCountdownToStartTimer()
+    {
+        return countdownTimer;
+    }
+    public bool IsGameOver()
+    {
+        return state == State.GameOver;
+
+    }
+    public float GetGameTimerNormalized()
+    {
+        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+}
+
+
