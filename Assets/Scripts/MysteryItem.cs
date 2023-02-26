6935887c54c4725f66a48a117da4c310b74a85ce
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-02-26 23:21:37 +0100

    add MysteryItem

---
+++ Assets/Scripts/MysteryItem.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class MysteryItem : MonoBehaviour
+{
+    public static MysteryItem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+
+        Transform visual = gameObject.transform.GetChild(Random.Range(0, gameObject.transform.childCount));
+        visual.gameObject.SetActive(true);
+
+
+
+
+
+
+
+    }
+    //private void Update()
+    //{
+    //    StartCoroutine(CoinMagnetCoroutine());
+
+    //}
+    public void ActivateCoinMagnet()
+    {
+        StartCoroutine(CoinMagnetCoroutine());
+    }
+
+    private IEnumerator CoinMagnetCoroutine()
+    {
+        //Debug.Log("Coin Magnet a1ctivated");
+        //float magnetRadius = 10f;
+        //float magnetDuration = 15000f;
+        //Collider[] colliders = Physics.OverlapSphere(Player.Instance.GetPosition(), magnetRadius);
+
+        //foreach (Collider collider in colliders)
+        //{
+        //    Debug.Log(collider.gameObject.layer + "eee");
+        //    if (collider.gameObject.layer == LayerMask.NameToLayer("Coin"))
+        //    {
+        //        CoinsCounter.Instance.IncreaseCoinsAmount();
+        //        collider.gameObject.SetActive(false);
+        //    }
+        //}
+
+        //yield return new WaitForSeconds(magnetDuration);
+
+        float magnetRadius = 12f;
+        float magnetDuration = 15f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < magnetDuration)
+        {
+            Collider[] colliders = Physics.OverlapSphere(Player.Instance.GetPosition(), magnetRadius);
+
+            foreach (Collider collider in colliders)
+            {
+                if (collider.gameObject.layer == LayerMask.NameToLayer("Coin"))
+                {
+                    CoinsCounter.Instance.IncreaseCoinsAmount();
+                    collider.gameObject.SetActive(false);
+                }
+            }
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+    }
+
+}
